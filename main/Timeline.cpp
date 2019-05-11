19ca57a4643c62fedae661cdbfaf40d37cf93fda
Daniel Rusu <rusudaniel39@gmail.com>
2019-05-11 03:12:16 +0100

    Timeline: formatted code

--- main/Timeline.cpp
+++ main/Timeline.cpp
@@ -1,51 +1,50 @@
 #include "Timeline.h"
+#include "esp_log.h"
 
 void
 Timeline::sync_time(void)
 {
-	lock_guard<mutex> lg(m);
+    lock_guard<mutex> lg(m);
 
-	timeinfo = {};
-    timeinfo.tm_hour = 0;
-    timeinfo.tm_mday = 0;
-    timeinfo.tm_mon = 0;
-    timeinfo.tm_year = 0;
-    timeinfo.tm_wday = 0;
-    timeinfo.tm_yday = 0;
+    timeinfo          = {};
+    timeinfo.tm_hour  = 0;
+    timeinfo.tm_mday  = 0;
+    timeinfo.tm_mon   = 0;
+    timeinfo.tm_year  = 0;
+    timeinfo.tm_wday  = 0;
+    timeinfo.tm_yday  = 0;
     timeinfo.tm_isdst = 0;
 
     int retry = 0;
+
     const int retry_count = 10;
-    while(timeinfo.tm_year < (2016 - 1900) && ++retry < retry_count) {
-        //printf("Waiting for system time to be set... (%d/%d)", retry, retry_count);
+    while (timeinfo.tm_year < (2016 - 1900) && (++retry < retry_count)) {
         vTaskDelay(2000 / portTICK_PERIOD_MS);
         time(&central_clock);
         start_offset = clock();
         localtime_r(&central_clock, &timeinfo);
     }
-
 }
 
 void
-Timeline::initialize_sntp(char *ip_addr)
+Timeline::initialize_sntp(char* ip_addr)
 {
     sntp_setoperatingmode(SNTP_OPMODE_POLL);
     sntp_setservername(0, ip_addr);
     sntp_init();
 }
 
-void 
+void
 Timeline::print_time()
 {
     char strftime_buf[64];
 
-    setenv("TZ", "CET-1CEST,M3.5.0,M10.5.0/3", 1); //rome
+    setenv("TZ", "CET-1CEST,M3.5.0,M10.5.0/3", 1); // Rome
     tzset();
     {
-    	lock_guard<mutex> lg(m);
-   		localtime_r(&central_clock, &timeinfo);
+        lock_guard<mutex> lg(m);
+        localtime_r(&central_clock, &timeinfo);
     }
     strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
-    printf("ROME/ITALY: %s", strftime_buf);
+    ESP_LOGI(tag, "ROME/ITALY: %s", strftime_buf);
 }
-
--- main/Timeline.h
+++ main/Timeline.h
@@ -1,69 +1,88 @@
 /*
- 	TIMELINE.h provides concurrent access to central clock for the board. central clock is keep updated by SNTP server.
-HOW TO CONFIGURE SNTP SERVER ON WINDOWS 10:
+    TIMELINE.h provides concurrent access to central clock for the board.
+central clock is keep updated by SNTP server. HOW TO CONFIGURE SNTP SERVER ON
+WINDOWS 10:
 - open regedit
-- navigate to HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\W32Time and locate "Start" registry key
+- navigate to HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\W32Time and
+locate "Start" registry key
 - Double click on "Start" and edit the value
 
 Change Startup type :
 Automatic - 2
 Manual - 3
 Disabled - 4
-Automatic (Delayed Start) - 2 
+Automatic (Delayed Start) - 2
 
-Note: When you change to Automatic (Delayed Start) a new key DelayedAutostart is created with value 1.
-When you change to Automatic from Automatic (Delayed Start), DelayedAutostart change value to 0.
+Note: When you change to Automatic (Delayed Start) a new key DelayedAutostart is
+created with value 1. When you change to Automatic from Automatic (Delayed
+Start), DelayedAutostart change value to 0.
 
 - change it to 2 (automatic)
 - open cmd with privileges
 - type commands:
 net start w32time
 
-reg add HKLM\system\CurrentControlSet\Services\W32Time\TimeProviders\NtpServer /v Enabled /t REG_DWORD /d 0x1 /f
-w32tm /config /syncfromflags:manual /reliable:yes /update
+reg add HKLM\system\CurrentControlSet\Services\W32Time\TimeProviders\NtpServer
+/v Enabled /t REG_DWORD /d 0x1 /f w32tm /config /syncfromflags:manual
+/reliable:yes /update
 
 net stop w32time
 
 net start w32time
 
 - now NTP server should answer requests at MACHINE_IP_ADDRESS at port 123
-- in case you see through wireshark that the machine does not reply to requests then disable windows firewall and retry
-
+- in case you see through wireshark that the machine does not reply to requests
+then disable windows firewall and retry
 */
-
+#ifndef TIMELINE_H_INCLUDED
+#define TIMELINE_H_INCLUDED
 #pragma once
 
-#include <cstdlib>
-#include <cstdio>
-#include <time.h>
-#include <iostream>
-#include "lwip/err.h" 
+#include "sdkconfig.h"
+
 #include "apps/sntp/sntp.h"
+
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
 #include <mutex>
 
-#define TIMESTAMP_VALIDATION 1514764801 //timestamp is valid if it's greater than 2018-01-01 
+// timestamp is valid if it's greater than 2018-01-01
+#define TIMESTAMP_VALIDATION 1514764801
 
-using namespace std;
+using std::lock_guard;
+using std::mutex;
 
-class Timeline{
-	clock_t start_offset; 
-	//this is an offset that has to be added to the server's clock to generate a valid timestamp for the packet
-	time_t central_clock;
+
+class Timeline {
+    // this is an offset that has to be added to the server's
+    // clock to generate a valid timestamp for the packet
+    clock_t start_offset;
+
+    time_t central_clock;
     struct tm timeinfo;
-    mutex m; //concurrent access to now variable
+
+    // concurrent access to timeinfo variable
+    mutex m; 
+
+    static constexpr char const* tag = "wwb-Timeline";
 
 public:
+    Timeline(){};
 
-	Timeline(){};
-	
-	void sync_time();
-	time_t get_time(){ 
-		lock_guard<mutex> lg(m);
-		return central_clock + ((clock() - start_offset)/((time_t)CLOCKS_PER_SEC)); 
-	}
-	void initialize_sntp(char *ip_addr);
-	void print_time();
+    void sync_time();
+    time_t get_time()
+    {
+        lock_guard<mutex> lg(m);
+        return central_clock +
+               ((clock() - start_offset) / ((time_t)CLOCKS_PER_SEC));
+    }
 
+    void initialize_sntp(char* ip_addr);
+    void print_time();
 };
 
-extern Timeline *system_timeline;
+extern Timeline* system_timeline;
+
+#endif // !TIMELINE_H_INCLUDED
