e088d83af748223a936fb6c1734d29bf6595025d
Daniel Rusu <rusudaniel39@gmail.com>
2019-05-12 02:02:02 +0100

    Packet: added checks before fetching data from buffer
    - better synchronization based on system time
    - changed printing to logging

--- main/Packet.cpp
+++ main/Packet.cpp
@@ -8,173 +8,186 @@
 #define MAC_LENGTH 6
 #define MAX_SSID_LENGTH 32
 
-int
-Packet::fetchData(void *buff, Timeline *time_offset){
+bool
+Packet::fetchData(void* buff)
+/*
+    1. check if packet is a probe request
+    2. check if current timestamp is valid
+    3. read packet fields from buffer
+*/
+{
+    if (isProbeReq(buff) == false) {
+        ESP_LOGV(tag, "(fetchData) not a probe request, discarding packet.");
+        return false;
+    }
 
-	const wifi_promiscuous_pkt_t *ppkt = (wifi_promiscuous_pkt_t *)buff;
-	const wifi_pkt_rx_ctrl_t pkt_ctrl = (wifi_pkt_rx_ctrl_t)ppkt->rx_ctrl; //contains CHANNEL,RSSI,TIMESTAMP
-	const wifi_frame_t *ipkt = (wifi_frame_t *)ppkt->payload;
-	const wifi_frame_hdr_t *hdr = &ipkt->hdr; //contains FRAME_CTRL,MAC,SEQUENCE_CTRL
+    time_t ts = 0;
+    time(&ts);
 
-	if((hdr->frame_ctrl^PROBE_REQ_ID)!=0)
-		return -1;
+    if (!Timeline::is_valid(ts)) {
+        ESP_LOGD(tag, "(fetchData) invalid timestamp, discarding packet.");
+        return false;
+    }
 
-	const wifi_frame_payload_t *network_data = (wifi_frame_payload_t *)ipkt->payload; //contains SSID and CHECKSUM	
-	
-	sequence_ctrl = static_cast<int>(hdr->sequence_ctrl);
-	rssi = static_cast<int>(pkt_ctrl.rssi);
-	timestamp = system_timeline->get_time();
-	channel = static_cast<int>(pkt_ctrl.channel);
+    const wifi_promiscuous_pkt_t* ppkt = (wifi_promiscuous_pkt_t*)buff;
 
-	int i,j;
-	for(i=0;i<MAC_LENGTH;i++)
-		mac_addr[i] = static_cast<char>(hdr->addr2[i]);
-	
+    // pkt_ctrl contains CHANNEL, RSSI, TIMESTAMP
+    const wifi_pkt_rx_ctrl_t pkt_ctrl = (wifi_pkt_rx_ctrl_t)ppkt->rx_ctrl;
+    const wifi_frame_t* ipkt          = (wifi_frame_t*)ppkt->payload;
 
-	ssid_length = static_cast<int>(network_data->data[1]);
-	for(i=2,j=0;(i<=(ssid_length+1))&&(j<MAX_SSID_LENGTH);i++,j++)
-		ssid[j] = network_data->data[i];
-	ssid[j] = '\0';
-	ssid_length = strlen(ssid);
+    // hdr contains FRAME_CTRL, MAC, SEQUENCE_CTRL
+    const wifi_frame_hdr_t* hdr = &ipkt->hdr;
 
-	hash = genHash(); 
+    // contains SSID and CHECKSUM
+    const wifi_frame_payload_t* network_data =
+        (wifi_frame_payload_t*)ipkt->payload;
 
-	return 0;
+    sequence_ctrl = static_cast<int>(hdr->sequence_ctrl);
+    rssi          = static_cast<int>(pkt_ctrl.rssi);
+    timestamp     = static_cast<int>(ts);
+    channel       = static_cast<int>(pkt_ctrl.channel);
+
+    int i, j;
+    for (i = 0; i < MAC_LENGTH; i++)
+        mac_addr[i] = static_cast<char>(hdr->addr2[i]);
+
+    ssid_length = static_cast<int>(network_data->data[1]);
+    for (i = 2, j = 0; (i <= (ssid_length + 1)) && (j < MAX_SSID_LENGTH);
+         i++, j++)
+        ssid[j] = network_data->data[i];
+    ssid[j]     = '\0';
+    ssid_length = strlen(ssid);
+
+    hash = genHash();
+
+    return true;
 }
 
 bool
-Packet::isProbeReq(void *buff){
-	const wifi_promiscuous_pkt_t *ppkt = (wifi_promiscuous_pkt_t *)buff;
-	const wifi_frame_t *ipkt = (wifi_frame_t *)ppkt->payload;
-	const wifi_frame_hdr_t *hdr = &ipkt->hdr; //contains FRAME_CTRL,MAC,SEQUENCE_CTRL
+Packet::isProbeReq(void* buff)
+{
+    const wifi_promiscuous_pkt_t* ppkt = (wifi_promiscuous_pkt_t*)buff;
+    const wifi_frame_t* ipkt           = (wifi_frame_t*)ppkt->payload;
+    // contains FRAME_CTRL,MAC,SEQUENCE_CTRL
+    const wifi_frame_hdr_t* hdr = &ipkt->hdr;
 
-	if((hdr->frame_ctrl^PROBE_REQ_ID)!=0)
-		return false;
+    if ((hdr->frame_ctrl ^ PROBE_REQ_ID) != 0)
+        return false;
 
-	return true;
+    return true;
 }
 
 void
-Packet::print(){
-
-	
-	printf("CH=%d, RSSI=%02d, MAC=%02x:%02x:%02x:%02x:%02x:%02x, SSID=%s, SEQ=%d, TIMESTAMP: %d, HASH=%s\n",
-				channel,
-				rssi,
-				mac_addr[0],mac_addr[1],mac_addr[2],
-				mac_addr[3],mac_addr[4],mac_addr[5],
-				ssid,
-				sequence_ctrl,
-				timestamp,
-				hash.c_str()
-			);
-	
-	
+Packet::debug_print()
+{
+    ESP_LOGD(
+        tag,
+        "ch: %d, rssi: %02d, mac: %02x:%02x:%02x:%02x:%02x:%02x, ssid: %s, "
+        "seq: %d, timestamp: %d, hash: %s\n",
+        channel, rssi, mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3],
+        mac_addr[4], mac_addr[5], ssid, sequence_ctrl, timestamp, hash.c_str());
 }
 
+std::string
+Packet::genHash()
+{
+    std::string hash;
 
-std::string 
-Packet::genHash(){
-	std::string hash;
+    std::ostringstream concat_data;
 
-	std::ostringstream concat_data; 
+    // concatenate fields in alphabetical order
+    concat_data << (channel) << (mac_addr[0]) << (mac_addr[1]) << (mac_addr[2])
+                << (mac_addr[3]) << (mac_addr[4]) << (mac_addr[5])
+                << (sequence_ctrl) << (ssid);
 
-	//concatenate fields in alphabetical order
-	concat_data <<(channel)
-				<<(mac_addr[0])<<(mac_addr[1])<<(mac_addr[2])<<(mac_addr[3])<<(mac_addr[4])<<(mac_addr[5])
-				<<(sequence_ctrl)
-				<<(ssid);
-				
-	hash = md5(concat_data.str());
+    hash = md5(concat_data.str());
 
-	return hash;
+    return hash;
 }
 
-void 
-Packet::serialize(char *buf){
+void
+Packet::serialize(char* buf)
+{
+    int* buf_int = (int*)buf;
 
-	int *buf_int = (int *)buf;
-	*buf_int = htonl(channel); //CHANNEL
-	buf_int++;
-	*buf_int = htonl(rssi); //RSSI
-	buf_int++;
-	*buf_int = htonl(sequence_ctrl); //SEQUENCE CTRL
-	buf_int++;
-	*buf_int = htonl(timestamp); //TIMESTAMP
-	buf_int++;
-	*buf_int = htonl(ssid_length); //SSID LENGTH
-	buf_int++;
+    *buf_int = htonl(channel); // CHANNEL
+    buf_int++;
+    *buf_int = htonl(rssi); // RSSI
+    buf_int++;
+    *buf_int = htonl(sequence_ctrl); // SEQUENCE CTRL
+    buf_int++;
+    *buf_int = htonl(timestamp); // TIMESTAMP
+    buf_int++;
+    *buf_int = htonl(ssid_length); // SSID LENGTH
+    buf_int++;
 
+    char* buf_c = (char*)buf_int;
 
-	char *buf_c = (char *)buf_int;
+    for (int i = 0; i < MAC_LENGTH; i++) {
+        *buf_c = mac_addr[i];
+        buf_c++;
+    }
 
-	for(int i=0; i < MAC_LENGTH; i++){
-		*buf_c = mac_addr[i];
-		buf_c++;
-	}
+    for (int i = 0; i < MD5_HASH_LENGTH; i++) {
+        *buf_c = hash.c_str()[i];
+        buf_c++;
+    }
 
-	for(int i=0; i < MD5_HASH_LENGTH; i++){
-		*buf_c = hash.c_str()[i];
-		buf_c++;
-	}
-
-	for(int i=0; i < ssid_length; i++){
-		*buf_c = ssid[i];
-		buf_c++;
-	}
+    for (int i = 0; i < ssid_length; i++) {
+        *buf_c = ssid[i];
+        buf_c++;
+    }
 }
 
-void 
-Packet::deserialize(char *buf){
-	int channel;
-	int rssi;
-	int sequence_ctrl;
-	int timestamp;
-	int ssid_length;
-	char mac_addr[MAC_LENGTH];
-	char ssid[MAX_SSID_LENGTH + 1];
-	char hash[MD5_HASH_LENGTH + 1];
+void
+Packet::deserialize(char* buf)
+{
+    int channel;
+    int rssi;
+    int sequence_ctrl;
+    int timestamp;
+    int ssid_length;
+    char mac_addr[MAC_LENGTH];
+    char ssid[MAX_SSID_LENGTH + 1];
+    char hash[MD5_HASH_LENGTH + 1];
 
-	int *buf_int = (int *)buf;
-	channel = ntohl(*buf_int); //CHANNEL
-	buf_int++;
-	rssi = ntohl(*buf_int); //RSSI
-	buf_int++;
-	sequence_ctrl = ntohl(*buf_int); //SEQUENCE CTRL
-	buf_int++;
-	timestamp = ntohl(*buf_int); //TIMESTAMP
-	buf_int++;
-	ssid_length = ntohl(*buf_int); //SSID LENGTH
-	buf_int++;
+    int* buf_int = (int*)buf;
 
-	char *buf_c = (char *)buf_int;
+    channel = ntohl(*buf_int); // CHANNEL
+    buf_int++;
+    rssi = ntohl(*buf_int); // RSSI
+    buf_int++;
+    sequence_ctrl = ntohl(*buf_int); // SEQUENCE CTRL
+    buf_int++;
+    timestamp = ntohl(*buf_int); // TIMESTAMP
+    buf_int++;
+    ssid_length = ntohl(*buf_int); // SSID LENGTH
+    buf_int++;
 
-	for(int i=0; i < MAC_LENGTH; i++){
-		mac_addr[i] = *buf_c;
-		buf_c++;
-	}
+    char* buf_c = (char*)buf_int;
 
-	for(int i=0; i < MD5_HASH_LENGTH; i++){
-		hash[i] = *buf_c;
-		buf_c++;
-	}
-	hash[MD5_HASH_LENGTH] = '\0';
+    for (int i = 0; i < MAC_LENGTH; i++) {
+        mac_addr[i] = *buf_c;
+        buf_c++;
+    }
 
-	for(int i=0; i < ssid_length; i++){
-		ssid[i] = *buf_c;
-		buf_c++;
-	}
-	ssid[ssid_length] = '\0';
+    for (int i = 0; i < MD5_HASH_LENGTH; i++) {
+        hash[i] = *buf_c;
+        buf_c++;
+    }
+    hash[MD5_HASH_LENGTH] = '\0';
 
-	printf("CH=%d, RSSI=%02d, MAC=%02x:%02x:%02x:%02x:%02x:%02x, SSID=%s, SEQ=%d, TIMESTAMP: %d, HASH=%s\n",
-			channel,
-			rssi,
-			(uint8_t)mac_addr[0],(uint8_t)mac_addr[1],(uint8_t)mac_addr[2],
-			(uint8_t)mac_addr[3],(uint8_t)mac_addr[4],(uint8_t)mac_addr[5],
-			ssid,
-			sequence_ctrl,
-			timestamp,
-			hash
-	);
+    for (int i = 0; i < ssid_length; i++) {
+        ssid[i] = *buf_c;
+        buf_c++;
+    }
+    ssid[ssid_length] = '\0';
 
+    ESP_LOGD(
+        tag,
+        "ch: %d, rssi: %02d, mac: %02x:%02x:%02x:%02x:%02x:%02x, ssid: %s, "
+        "seq: %d, timestmap: %d, hash: %s\n",
+        channel, rssi, (uint8_t)mac_addr[0], (uint8_t)mac_addr[1],
+        (uint8_t)mac_addr[2], (uint8_t)mac_addr[3], (uint8_t)mac_addr[4],
+        (uint8_t)mac_addr[5], ssid, sequence_ctrl, timestamp, hash);
 }
--- main/Packet.h
+++ main/Packet.h
@@ -1,8 +1,12 @@
+#ifndef PACKET_H_INCLUDED
+#define PACKET_H_INCLUDED
 #pragma once
+
+#include "esp_log.h"
 #include "esp_wifi_types.h"
-#include <string.h>
+
 #include "lwip/sockets.h"
-#include <string>
+
 #include <iostream>
 #include <sstream>
 
@@ -16,61 +20,64 @@
 using namespace std;
 
 typedef struct {
-	unsigned frame_ctrl:16;
-	unsigned duration_id:16;
-	uint8_t addr1[6]; /* receiver address */
-	uint8_t addr2[6]; /* sender address */
-	uint8_t addr3[6]; /* filtering address */
-	unsigned sequence_ctrl:16;
-	//uint8_t addr4[6]; /* optional */
+    unsigned frame_ctrl : 16;
+    unsigned duration_id : 16;
+    uint8_t addr1[6]; /* receiver address */
+    uint8_t addr2[6]; /* sender address */
+    uint8_t addr3[6]; /* filtering address */
+    unsigned sequence_ctrl : 16;
+    // uint8_t addr4[6]; /* optional */
 } wifi_frame_hdr_t;
 
 typedef struct {
-	uint8_t data[0];
-	uint32_t crc;
+    uint8_t data[0];
+    uint32_t crc;
 } wifi_frame_payload_t;
 
 typedef struct {
-	wifi_frame_hdr_t hdr;
-	uint8_t payload[0]; /* network data ended with 4 bytes csum (CRC32) */
+    wifi_frame_hdr_t hdr;
+    uint8_t payload[0]; /* network data ended with 4 bytes csum (CRC32) */
 } wifi_frame_t;
 
-class Packet{
-	int channel;
-	int rssi;
-	int sequence_ctrl;
-	int timestamp;
-	int ssid_length;
-	char mac_addr[MAC_LENGTH];
-	char ssid[MAX_SSID_LENGTH + 1];
-	string hash;
+class Packet {
+    int channel;
+    int rssi;
+    int sequence_ctrl;
+    int timestamp;
+    int ssid_length;
+    char mac_addr[MAC_LENGTH];
+    char ssid[MAX_SSID_LENGTH + 1];
+    string hash;
 
-	std::string genHash();
+    // logging tag
+    static constexpr char const* tag = "wwb-Packet";
+
+private:
+    std::string genHash();
+
 public:
+    Packet() { hash = "test packet"; };
+    Packet(string s) : hash(s){};
 
-	Packet() { hash = "test packet"; };
-	Packet(string s): hash(s) { };
+    /* Returns false if packet is not a probe request or invalid timestamp */
+    bool fetchData(void* buff);
 
-	string temp_print(){
-		return hash;
-	}
-	int fetchData(void *buff, Timeline *time_offset);
-	void print();
-	static bool isProbeReq(void *buff);
-	int send(int socket);
-	bool operator<(const Packet& p) const {
-   		return this->hash < p.hash;
-	}
+    static bool isProbeReq(void* buff);
 
-	int get_timestamp(){
-		return timestamp;
-	}
+    int get_packet_size()
+    {
+        return 5 * sizeof(int) +
+               (ssid_length + MD5_HASH_LENGTH + MAC_LENGTH) * sizeof(char);
+    }
 
-	int get_packet_size(){
-		return 5*sizeof(int) + (ssid_length+MD5_HASH_LENGTH+MAC_LENGTH)*sizeof(char);
-	}
+    bool operator<(const Packet& p) const { return this->hash < p.hash; }
 
-	void serialize(char *buf);
-	void deserialize(char *buf);
+    void serialize(char* buf);
+    void deserialize(char* buf);
 
+    void debug_print();
+    // string temp_print() { return hash; }
+    // int get_timestamp() { return timestamp; }
 };
+
+#endif
